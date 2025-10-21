97b1822ae1c6c79823e5a81ba968db3df90e0517
Francisco Javier Inzunza Pacheco <FranciscoInzunza2006@outlook.com>
2025-10-21 15:19:17 -0700

    Sudoku elements index using dataset property

--- JavaScript/Sudoku/src/main.js
+++ JavaScript/Sudoku/src/main.js
@@ -3,16 +3,28 @@
 
 console.log("Hello World!")
 
+const debug = document.getElementById("debug_output");
+
+function block_position(event) {
+    let block_element = event.target;
+    debug.innerText += "Block index: " + block_element.dataset.index + "\n";
+    debug.innerText += "Region index: " + block_element.parentElement.dataset.index + "\n";
+}
+
 function init_sudoku() {
     let sudoku_grid = document.getElementById("sudoku_grid");
 
-    for (let region = 0; region < 9; region++) {
+    for (let region_index = 0; region_index < 9; region_index++) {
         let sudoku_region = document.createElement("div");
+        sudoku_region.dataset.index = region_index.toString();
         sudoku_region.classList.add("sudoku-region");
 
-        for (let block = 0; block < 9; block++) {
+        for (let block_index = 0; block_index < 9; block_index++) {
             let sudoku_block = document.createElement("div");
-            sudoku_block.innerText = (block + 1).toString();
+            sudoku_block.dataset.index = block_index.toString();
+            sudoku_block.onclick = block_position;
+
+            sudoku_block.innerText = (block_index + 1).toString();
 
             sudoku_region.appendChild(sudoku_block);
         }
