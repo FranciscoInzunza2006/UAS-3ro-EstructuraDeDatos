d886830d53545c1dcc4d021982a1649e341027fd
Francisco Javier Inzunza Pacheco <FranciscoInzunza2006@outlook.com>
2025-10-26 13:49:57 -0700

    Ok, stick to the 3x3

--- JavaScript/Sudoku/src/sudoku.js
+++ JavaScript/Sudoku/src/sudoku.js
@@ -1,4 +1,65 @@
 
+class SudokuGenerator {
+    static DEFAULT_SUDOKU_WIDTH = 3
+    static DEFAULT_SUDOKU_HEIGHT = 3
+
+    static DEFAULT_REGION_WIDTH = 3
+    static DEFAULT_REGION_HEIGHT = 3
+
+    static EMPTY_CELL = 0;
+
+    constructor(sudoku_width, sudoku_height, region_width, region_height) {
+        this.sudoku_width = sudoku_width;
+        this.sudoku_height = sudoku_height;
+
+        this.region_width = region_width;
+        this.region_height = region_height;
+    }
+
+    get rows() {
+        return this.sudoku_height * this.region_height;
+    }
+
+    get columns() {
+        return this.sudoku_width * this.region_width;
+    }
+
+    generate() {
+        // Empty sudoku
+        let sudoku = new Array(this.rows);
+        for (let row = 0; row < ROWS; row++) {
+            sudoku[row] = new Array(this.columns).fill(EMPTY_CELL);
+        }
+
+        // Fill diagonal regions
+        for (let region_index = 0; region_index < this.region_width; region_index++) {
+            const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+            shuffle(nums);
+            for (let row = 0; row < this.region_height; row++) {
+                for (let col = 0; col < this.region_width; col++) {
+                    sudoku[region_index * this.region_height + row][region_index * this.region_width + col] = nums.pop();
+                }
+            }
+        }
+
+        solveSudoku(sudoku);
+
+        return sudoku;
+    }
+
+    #shuffle_array(array) {
+        let current_index = array.length-1;
+
+        while (current_index > 0) {
+            const random_index = Math.floor(Math.random() * (current_index+1));
+            [array[current_index], array[random_index]] = [array[random_index], array[current_index]];
+
+            current_index--;
+        }
+    }
+}
+
 const EMPTY_CELL = 0;
 
 // SUDOKU DIMENSIONS
@@ -17,27 +78,7 @@ function shuffle(array) {
 }
 
 function generateSudoku() {
-    // Empty sudoku
-    let sudoku = new Array(ROWS);
-    for (let row = 0; row < ROWS; row++) {
-        sudoku[row] = new Array(COLUMNS).fill(EMPTY_CELL);
-    }
 
-    // Fill diagonal regions
-    for (let region_index = 0; region_index < 3; region_index++) {
-        const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        shuffle(nums);
-
-        for (let row = 0; row < 3; row++) {
-            for (let col = 0; col < 3; col++) {
-                sudoku[region_index * 3 + row][region_index * 3 + col] = nums.pop();
-            }
-        }
-    }
-
-    solveSudoku(sudoku);
-
-    return sudoku;
 }
 
 function generateSudokuPuzzle(solved_sudoku, difficulty = 35) {
