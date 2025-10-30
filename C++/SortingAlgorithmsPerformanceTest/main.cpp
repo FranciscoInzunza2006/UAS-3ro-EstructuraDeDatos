30d2c77c0978a1433e248e1df51e9091097a41cd
Francisco Javier Inzunza Pacheco <FranciscoInzunza2006@outlook.com>
2025-10-29 17:27:18 -0700

    Separated into two functions not for many reasons lot of reasons

--- C++/SortingAlgorithmsPerformanceTest/main.cpp
+++ C++/SortingAlgorithmsPerformanceTest/main.cpp
@@ -11,43 +11,60 @@
 #include "sorting_algorithms.hpp"
 #include "sorting_benchmark.hpp"
 
+#define CELL_WIDTH 14
+
 using NamedArrayFunction = std::pair<const std::string, ArrayFunction>;
 
-void printBenchmarkResults(const std::vector<BenchmarkResults>& results, const std::vector<std::string>& sorting_algorithms_name,
+void printBenchmarkResult(const BenchmarkResults& result, const std::string& algorithm_name,
+                          const std::vector<std::string>& input_generators_name,
+                          const std::vector<std::size_t>& sample_sizes)
+{
+    constexpr auto COLOR_ALGORITHM = "\033[0;96m";
+    constexpr auto COLOR_HEADER = "\033[0;95m";
+    // ReSharper disable once CppTooWideScope
+    constexpr auto COLOR_TIME = "\033[0;93m";
+    constexpr auto COLOR_RESET = "\033[0m";
+
+    std::stringstream results_table;
+    results_table << COLOR_ALGORITHM << std::left << std::setw(CELL_WIDTH) << algorithm_name;
+
+    // Sample size
+    for (const std::size_t& sample_size : sample_sizes)
+    {
+        results_table << COLOR_HEADER << std::right << std::setw(CELL_WIDTH) << sample_size;
+    }
+    results_table << "\n";
+
+    // Samples
+    for (std::size_t type = 0; type < input_generators_name.size(); type++)
+    {
+        results_table << COLOR_HEADER << std::left << std::setw(CELL_WIDTH) << input_generators_name[type];
+
+        // Benchmark result
+        for (size_t size = 0; size < sample_sizes.size(); size++)
+        {
+            results_table << COLOR_TIME << std::right << std::setw(CELL_WIDTH) << result[type][size].count();
+        }
+        results_table << "\n";
+    }
+
+    std::cout << results_table.str() << COLOR_RESET << std::endl;
+}
+
+void printBenchmarkResults(const std::vector<BenchmarkResults>& results,
+                           const std::vector<std::string>& sorting_algorithms_name,
                            const std::vector<std::string>& input_generators_name,
                            const std::vector<std::size_t>& sample_sizes)
 {
+    // Header
+    // std::cout << std::setw(CELL_WIDTH) << "Ordenamiento: " << std::setw(CELL_WIDTH) << ' ' << std::setw(CELL_WIDTH) <<
+    //     "Tamaño de muestra" << '\n';
+
     // Print results
     for (std::size_t algorithm = 0; algorithm < sorting_algorithms_name.size(); algorithm++)
     {
-        constexpr int cell_width = 14;
-        const std::string& algorithm_name = sorting_algorithms_name[algorithm];
-
-        // Header
-        std::stringstream results_table;
-
-        results_table << std::left << std::setw(cell_width) << algorithm_name;
-        for (const std::size_t& sample_size : sample_sizes)
-        {
-            results_table << std::right << std::setw(cell_width) << sample_size;
-        }
-        results_table << "\n";
-
-        // Samples
-        for (std::size_t type = 0; type < input_generators_name.size(); type++)
-        {
-            const std::string& sample_type = input_generators_name[type];
-            results_table << std::left << std::setw(cell_width) << sample_type;
-
-            // Time
-            for (size_t size = 0; size < sample_sizes.size(); size++)
-            {
-                results_table << std::right << std::setw(cell_width) << results[algorithm][type][size].count();
-            }
-            results_table << "\n";
-        }
-
-        std::cout << results_table.str() << std::endl;
+        printBenchmarkResult(results[algorithm], sorting_algorithms_name[algorithm], input_generators_name,
+                             sample_sizes);
     }
 }
 
