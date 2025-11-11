e91cd1be407bc4b7b42e3bdb1ccdff92aafccc37
Francisco Javier Inzunza Pacheco <FranciscoInzunza2006@outlook.com>
2025-11-11 00:57:11 -0700

    Double circular

---
+++ Java/linked-lists/doubly_circular_linked.java
@@ -0,0 +1,272 @@
+import java.util.Scanner;
+
+class DriverCode {
+    static final String SEPARATOR = "-------------------------------------------------";
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+        do {
+            System.out.println(SEPARATOR);
+            System.out.println("Lista circular doble");
+            System.out.println(SEPARATOR);
+            list.display();
+            System.out.println(SEPARATOR);
+            System.out.println("1. Insertar elemento al inicio");
+            System.out.println("2. Insertar elemento al final");
+            System.out.println("3. Insertar elemento en una posicion especifica");
+            System.out.println("4. Eliminar elemento al inicio");
+            System.out.println("5. Eliminar elemento al final");
+            System.out.println("6. Eliminar elemento en una posicion especifica");
+            System.out.println("7. Buscar elemento en la lista");
+            System.out.println("8. Salir");
+            System.out.println(SEPARATOR);
+
+            System.out.print("Ingresa una opcion: ");
+            choice = sc.nextInt();
+
+            System.out.println(SEPARATOR);
+            int val = -1;
+            int index = -1;
+            switch (choice) {
+                case 1:
+                    System.out.print("Ingresa un valor: ");
+                    val = sc.nextInt();
+
+                    list.pushFirst(val);
+                    break;
+                case 2:
+                    System.out.print("Ingresa un valor: ");
+                    val = sc.nextInt();
+
+                    list.pushLast(val);
+
+                    break;
+                case 3:
+                    System.out.print("Ingresa un valor: ");
+                    val = sc.nextInt();
+
+                    System.out.print("Ingresa el indice deseado: ");
+                    index = sc.nextInt();
+
+                    list.pushAt(val, index);
+                    break;
+
+                case 4:
+                    list.popFirst();
+                    break;
+
+                case 5:
+                    list.popLast();
+                    break;
+                case 6:
+                    System.out.print("Ingresa el indice deseado: ");
+                    index = sc.nextInt();
+
+                    list.popAt(index);
+                    break;
+                case 7:
+                    System.out.print("Ingresa un valor: ");
+                    val = sc.nextInt();
+
+                    list.search(val);
+                    break;
+
+                case 8:
+                    System.out.println("Adios.");
+                    break;
+
+                default:
+                    System.out.println("Opcion invalida.");
+                    break;
+            }
+        } while (choice != 8);
+
+        sc.close();
+    }
+}
+
+class LinkedList {
+    Node head = null;
+
+    // Push
+    public void pushFirst(final int value) {
+        System.out.println("Se inserto el nodo.");
+        if (head == null) {
+            head = new Node(value, null, null);
+            head.next = head;
+            head.previous = head;
+
+            return;
+        }
+
+        Node new_head = new Node(value, head, head.previous);
+        head.previous.next = new_head;
+        head.previous = new_head;
+        head = new_head;
+    }
+
+    public void pushLast(final int value) {
+        if (head == null) {
+            pushFirst(value);
+            return;
+        }
+
+        Node tail = head.previous;
+        tail.next = new Node(value, head, tail);
+        head.previous = tail.next;
+
+        System.out.println("Se inserto el nodo.");
+    }
+
+    public void pushAt(final int value, final int index) {
+        if (index == 0) {
+            pushFirst(value);
+            return;
+        }
+
+        if (head == null) {
+            System.out.println("No se pudo insertar el nodo.");
+            return;
+        }
+
+        Node current_node = head.next;
+        int i = 1;
+        while (i < index && current_node != head) {
+            current_node = current_node.next;
+            i++;
+        }
+        if (i < index) {
+            System.out.println("No se pudo insertar el nodo.");
+            return;
+        }
+
+        System.out.println("Se inserto el nodo.");
+        Node new_node = new Node(value, current_node, current_node.previous);
+        current_node.previous.next = new_node;
+        current_node.previous = new_node;
+    }
+
+    // Pop
+    public void popFirst() {
+        if (head == null) {
+            System.out.println("La lista esta vacia.");
+            return;
+        }
+
+        if (head.next == head) {
+            head = null;
+            return;
+        }
+
+        System.out.println("Se elimino el nodo.");
+        head.next.previous = head.previous;
+        head.previous.next = head.next;
+        head = head.next;
+    }
+
+    public void popLast() {
+        if (head == null) {
+            System.out.println("La lista esta vacia.");
+            return;
+        }
+
+        System.out.println("Se elimino el nodo.");
+        if (head.next == head) {
+            head = null;
+            return;
+        }
+
+        Node removed_tail = head.previous;
+        removed_tail.previous.next = head;
+        head.previous = removed_tail.previous;
+    }
+
+    public void popAt(final int index) {
+        if (head == null) {
+            System.out.println("La lista esta vacia.");
+            return;
+        }
+
+        if (index == 0) {
+            popFirst();
+            return;
+        }
+
+        Node current_node = head.next;
+        int i = 1;
+        while (i < index && current_node != head) {
+            current_node = current_node.next;
+            i++;
+        }
+
+        if (current_node == head || i < index) {
+            System.out.println("No se pudo eliminar el nodo.");
+            return;
+        }
+        System.out.println("Se elimino el nodo.");
+        if (current_node.next == head) {
+
+            current_node.previous.next = head;
+            head.previous = current_node.previous;
+        } else {
+
+            current_node.previous.next = current_node.next;
+            current_node.next.previous = current_node.previous;
+        }
+    }
+
+    // Extra
+    public void display() {
+        if (head == null) {
+            System.out.println("La lista esta vacia.");
+            return;
+        }
+
+        Node current_node = head;
+        do {
+            System.out.printf("%d ", current_node.value);
+            current_node = current_node.next;
+        } while (current_node != head);
+        System.out.println();
+    }
+
+    public void search(final int needle) {
+        if (head == null) {
+            System.out.println("La lista esta vacia.");
+            return;
+        }
+
+        boolean found = false;
+        int i = 0;
+        Node current_node = head;
+        do {
+            if (current_node.value == needle) {
+                found = true;
+                System.out.printf("El valor esta en el nodo con el indice %d\n", i);
+            }
+
+            current_node = current_node.next;
+            i++;
+        } while (current_node != head);
+
+        if (!found) {
+            System.out.println("El valor buscado no esta en la lista.");
+        }
+    }
+
+    // Node definition
+    class Node {
+        public int value;
+        public Node next;
+        public Node previous;
+
+        public Node(int value, Node next, Node previous) {
+            this.value = value;
+            this.next = next;
+            this.previous = previous;
+        }
+    }
+}
