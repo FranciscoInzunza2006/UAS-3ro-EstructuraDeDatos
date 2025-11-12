6ff371b963c25a2bf5b93772a026a29379058a2d
Francisco Javier Inzunza Pacheco <FranciscoInzunza2006@outlook.com>
2025-11-11 18:25:00 -0700

    Queue Java list implementation

--- Java/queues/queue_list.java
+++ Java/queues/queue_list.java
@@ -18,7 +18,8 @@ abstract class DriverCode {
             System.out.println("2. Eliminar un elemento");
             System.out.println("3. Salir");
             printSeparator();
-
+            option = getInt("Ingresa la opcion deseada: ");
+            printSeparator();
             switch (option) {
                 case 1:
                     queue.enqueue(getInt("Ingresa el valor que quieres insertar: "));
@@ -37,7 +38,10 @@ abstract class DriverCode {
                     break;
             }
         }
+        
+        scanner.close();
     }
+
 
     private static int getInt(final String prompt) {
         System.out.print(prompt);
@@ -51,21 +55,54 @@ abstract class DriverCode {
 
 class Queue {    
     class Node {
-        int value;
-        Node next;
+        public int value;
+        public Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
     }
     Node queue = null;
 
-    public boolean enqueue(final int val) {
+    public boolean enqueue(final int value) {
+        if (queue == null) {
+            queue = new Node(value, null);
+            return true;
+        }
+
+        Node tail = queue;
+        while (tail.next != null)
+            tail = tail.next;
+        tail.next = new Node(value, null);
 
         return true;
     }
 
-    public boolean dequeue() {
-        return true;
+    public int dequeue() {
+        if (queue == null) {
+            System.out.println("La cola esta vacía.");
+            return -1;
+        }
+
+        int val = queue.value;
+        queue = queue.next;
+        System.err.printf("Se sacó el valor %d de la cola .\n", val);
+
+        return val;
     }
 
     public void display() {
+        if (queue == null) {
+            System.out.println("La cola esta vacía.");
+            return;
+        }
 
+        Node current_node = queue;
+        while (current_node != null) {
+            System.out.printf("%d ", current_node.value);
+            current_node = current_node.next;
+        }
+        System.out.println();
     }
 }
