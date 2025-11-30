b5b246b59856e3dfa8450be85e8b756c4a670785
Francisco Javier Inzunza Pacheco <FranciscoInzunza2006@outlook.com>
2025-11-29 18:13:01 -0700

    Some advances in this... API? Library?

--- C++/Binary Search Tree/modules/command_line/command_line.cpp
+++ C++/Binary Search Tree/modules/command_line/command_line.cpp
@@ -2,60 +2,142 @@
 // Created by Franc on 29/11/2025.
 //
 
+#include <functional>
 #include <iostream>
+#include <utility>
 #include <vector>
+
+typedef std::pair<std::string, std::string_view> StringPair;
 
 class Command
 {
+public:
+    StringPair command;
+    std::function<void()> callback;
+    std::vector<StringPair> params;
+
+    void printHelp() const
+    {
+        std::cout << command.first << ' ';
+        for (const auto & param : params)
+        {
+            std::cout << '<' << param.first << "> ";
+        }
+        std::cout << '\n' << command.second << '\n';
+
+        for (const auto & param : params)
+        {
+            std::cout << "\t" << param.first << " : " << param.second << "\n";
+        }
+    }
+
+    Command() = default;
+
+    Command(StringPair command, std::function<void()> callback, const std::vector<StringPair>& params = {}) :
+        command(std::move(command)), callback(std::move(callback)), params(params)
+    {
+    };
 };
+
 
 class CommandLine
 {
+    std::vector<Command> commands;
+
     static std::vector<std::string> split(const std::string_view str)
     {
-        std::vector<std::string> result;
+        std::vector<std::string> tokens;
 
-        int i = 0;
         bool in_word = false;
         for (const char c : str)
         {
-            if (c != ' ')
+            if (c == ' ')
+            {
+                in_word = false;
+                continue;
+            }
+
+            if (!in_word)
             {
                 in_word = true;
-
-                if (result.size() == i) result.emplace_back("");
-                result[i].push_back(c);
+                tokens.emplace_back();
             }
-            else
-            {
-                if (in_word)
-                {
-                    in_word = false;
-                    i++;
-                }
-            }
+            tokens.back().push_back(c);
         }
-        return result;
+        return tokens;
     }
+
+    bool runCommand(const Command& command, std::string_view str);
 
     bool parseCommand(const std::string_view input)
     {
-        auto foo = split(input);
+        const std::vector<std::string> tokens = split(input);
+
+        constexpr int COMMAND = 0;
+
+        if (tokens[COMMAND] == "exit")
+        {
+            running = false;
+            return true;
+        }
+
+        for (const Command& command : commands)
+        {
+            if (command.command.first == tokens[COMMAND])
+            {
+                command.callback();
+                return true;
+            }
+        }
+
+        return true;
     }
 
 public:
+    explicit CommandLine(const std::vector<Command>& commands) : commands(commands)
+    {
+    }
+
+    bool running = true;
+
     bool read()
     {
         std::string input;
+        //input = "   insert 10    ";
         std::getline(std::cin, input);
 
-        parseCommand(input);
-        return true;
+        return parseCommand(input);
     }
 };
 
+bool foo()
+{
+    std::cout << "Hello, World!\n";
+    return true;
+}
+
 int main()
 {
-    CommandLine cmd;
-    cmd.read();
+    const std::vector commands = {
+        Command({"insert", "Inserta un número en el árbol."}, &foo, {
+            {"Valor", "El valor que se va a ingresar, no debe estár en el árbol."}
+        }),
+        Command({"search", "Busca un número en el árbol y muestra su ruta."}, &foo, {
+            {"Valor", "El valor que se va a buscar."}
+        }),
+        Command({"delete", "Elimina un número del árbol."}, &foo),
+        Command({"inorder", "Inserta un número en el árbol."}, &foo),
+        Command({"preorder", "Inserta un número en el árbol."}, &foo),
+        Command({"postorder", "Inserta un número en el árbol."}, &foo),
+        Command({"height", "Inserta un número en el árbol."}, &foo),
+        Command({"size", "Inserta un número en el árbol."}, &foo),
+        Command({"export", "Inserta un número en el árbol."}, &foo),
+    };
+    CommandLine cmd(commands);
+
+    while (cmd.running)
+    {
+        std::cout << ">> ";
+        cmd.read();
+    }
 }
