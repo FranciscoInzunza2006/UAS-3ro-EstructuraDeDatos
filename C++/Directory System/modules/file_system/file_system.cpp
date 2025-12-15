7b63edb084e30790b5730aa3bd6aef5821fde007
Francisco Javier Inzunza Pacheco <FranciscoInzunza2006@outlook.com>
2025-12-15 08:45:17 -0700

    Setup for presentation

--- C++/Directory System/modules/file_system/file_system.cpp
+++ C++/Directory System/modules/file_system/file_system.cpp
@@ -12,19 +12,19 @@
 FileSystemUI::FileSystemUI() : cmd(createCommands())
 {
     // Some basic structure for testing
-    const auto documents = new Folder("Documents", system.root_directory);
-    new File("Homework v2.pdf", documents);
-    new Folder("CBTIS 45", documents);
-
-    new Folder("Images", system.root_directory);
-    new Folder("Videos", system.root_directory);
-    new Folder("3D models", system.root_directory);
-
-    new File("Homework.pdf", system.trash_bin);
-
-    Folder* c = new Folder("C", new Folder("B", new Folder("A", system.root_directory)));
-    for (int i = 0; i < 5; i++)
-        new File(std::to_string(i) + ".txt", c);
+    // const auto documents = new Folder("Documents", system.root_directory);
+    // new File("Homework v2.pdf", documents);
+    // new Folder("CBTIS 45", documents);
+    //
+    // new Folder("Images", system.root_directory);
+    // new Folder("Videos", system.root_directory);
+    // new Folder("3D models", system.root_directory);
+    //
+    // new File("Homework.pdf", system.trash_bin);
+    //
+    // Folder* c = new Folder("C", new Folder("B", new Folder("A", system.root_directory)));
+    // for (int i = 0; i < 5; i++)
+    //     new File(std::to_string(i) + ".txt", c);
 
 
     system.root_directory->showContents();
@@ -217,9 +217,9 @@ void FileSystemUI::run()
     {
         std::cout << system.getWorkingDirectory()->getPath() << "/> ";
 
-        //std::string input = customGetLine();
-        std::string input;
-        std::getline(std::cin, input);
+        std::string input = customGetLine();
+        //std::string input;
+        //std::getline(std::cin, input);
 
         cmd.processInput(input);
         std::cout << std::endl;
